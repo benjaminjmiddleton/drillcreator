0f3554ea1cebf28b20733e4e737e5e522e73a9ba
Matthew Kuhr <65468012+GameGeek720@users.noreply.github.com>
2021-10-19 09:35:35 -0400

    Initial Drill Solver
    Cycles through all permutations and ranks results

---
+++ src/Drill Solver.py
@@ -0,0 +1,45 @@
+from itertools import permutations
+
+def getPermutations(s):
+    return permutations(s)
+    
+    
+def rankTransition(s1, s2, counts):
+    def calculateDistance(a,b):
+        return ((b[0] - a[0])**2 + (b[1] - a[1])**2)**.5
+        
+    def calculateCollisions(s1, s2, counts):
+        def updatePlayer(t1, t2, c):
+            return (t1[0] + (t2[0] - t1[0]) / c, t1[1] + (t2[1] - t1[1]) / c)
+            
+        collisionCount = 0
+        for i in range(counts,0,-1):
+            for j in range(len(s1)):
+                for k in range(j):
+                    d = calculateDistance(s1[j],s1[k])
+                    if d < 1:
+                        collisionCount += 1;
+                    
+            s1 = [updatePlayer(s1[p], s2[p], i) for p in range(min(len(s1),len(s2)))]
+        return collisionCount;
+        
+    distance = 0
+    maxDistance = 0    
+    for i in range(min(len(s1), len(s2))):
+        d = calculateDistance(s1[i], s2[i])
+        distance += d
+        maxDistance = max(d, maxDistance)
+    collisions = calculateCollisions(s1, s2, counts)
+    score = distance + maxDistance**2 + collisions*100
+    return (score, s2)
+    
+set1 = [(0,0),(8,0),(0,8),(8,8)]
+set2 = [(0,0),(8,0),(0,8),(8,8)]
+
+p = getPermutations(set2)
+
+rankings = [rankTransition(set1,s2,8) for s2 in getPermutations(set2)]
+rankings.sort(key=lambda tup: tup[0])
+
+for r in rankings:
+    print(r)
