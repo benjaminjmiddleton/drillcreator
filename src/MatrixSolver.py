b1f774859b47a16f6cceb85da6f1f89d6f0c9dae
Mason Davy <massonnosam11@gmail.com>
2021-10-20 07:35:19 -0400

    Still screwing around

--- src/MatrixSolver.py
+++ src/MatrixSolver.py
@@ -1,9 +1,20 @@
+def distance(pt0: tuple[float, float], pt1: tuple[float, float]) -> float:
+    """Computes the distance between two points"""
+    return abs(((pt0[0] - pt1[0]) * (pt0[0] - pt1[0])) + ((pt0[1] - pt1[1]) * (pt0[1] - pt1[1])))
+
 def getMatrixPermutations(set0: tuple[tuple[float, float], ...],
                           set1: tuple[tuple[float, float], ...],
                           cutoff: float) -> tuple[tuple[float, float, float], ...]:
     """Let's try something"""
 
-    # First let's validate our inputs and get the count
+    # First let's validate our inputs
     assert(set0.count == set1.count)
     assert(cutoff >= 0)
-    nPoints = set0.count
+
+    # Now we'll build our matrix (just using lists for now, may consider something more optimal in the future)
+    matrix = []
+    for start in set0:
+        rows = []
+        for end in set1:
+            rows.append(distance(start, end))
+        matrix.append(rows)
