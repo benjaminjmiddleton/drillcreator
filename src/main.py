05c2bcb9dbc0dbad05fb0446aa932c177dccba35
Benjamin Middleton <bjamesmiddleton@gmail.com>
2021-11-30 13:51:47 -0500

    pdf version of capstone assessment

--- src/main.py
+++ src/main.py
@@ -1,21 +1,41 @@
-"""PyQt5 Hello World"""
+"""PyQt5 Application"""
 
 import sys
 
-# you will need to 'pip install pyqt5' in a venv or globally.
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QMainWindow
+
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        self.axes.set_axis_off()
+        super(MplCanvas, self).__init__(fig)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi('ui/MainWindow.ui', self)
+        
+        sc = MplCanvas(self, width=5, height=4, dpi=100)
+        img = plt.imread('ui/field.png')
+        sc.axes.imshow(img)
+        sc.axes.plot(300, 100, 'rx')
+        sc.axes.plot(500, 100, 'gx')
+        sc.axes.plot(300, 300, 'bx')
+        sc.axes.plot(500, 300, 'yx')
+        self.setCentralWidget(sc)
+
+        self.show()
 
 app = QApplication(sys.argv)
-
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
-
-window.show()
-
-sys.exit(app.exec_())
+window = MainWindow()
+app.exec_()
