de667530d3d113f17bfe3b5b617a372e2b58158b
Benjamin Middleton <bjamesmiddleton@gmail.com>
2022-02-07 20:32:29 -0500

    cleanup

--- src/Show.py
+++ src/Show.py
@@ -4,7 +4,7 @@ class Show:
     def __init__(self, performers, drillsets=[]):
         """
         @param marchers: list of Performer objects that will be used in the show
-        @param drillsets: list of drillsets the show will have (can be empty for construction)
+        @param drillsets: list of drillsets the show will have
         """
         self.performers = performers
         self.drillsets = drillsets
